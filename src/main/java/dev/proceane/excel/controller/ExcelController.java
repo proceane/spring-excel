17ffda8df053dff2d08800988348ca42d8899c1f
proceane <proceane1@gmail.com>
2023-02-10 15:09:45 +0900

    encrypt excel file

--- src/main/java/dev/proceane/excel/controller/ExcelController.java
+++ src/main/java/dev/proceane/excel/controller/ExcelController.java
@@ -1,8 +1,8 @@
 package dev.proceane.excel.controller;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-
+import dev.proceane.excel.service.ExcelService;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ContentDisposition;
@@ -12,8 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import dev.proceane.excel.service.ExcelService;
-import lombok.RequiredArgsConstructor;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,8 +20,9 @@ public class ExcelController {
 
     private final ExcelService service;
 
+    @SneakyThrows
     @GetMapping("/excel")
-    public ResponseEntity<Resource> downloadExcelFile() throws IOException {
+    public ResponseEntity<Resource> downloadExcelFile() {
         InputStreamResource resource = new InputStreamResource(service.getExcelFile());
         return ResponseEntity.ok()
                              .header(HttpHeaders.CONTENT_DISPOSITION, ContentDisposition.attachment()
@@ -30,6 +30,18 @@ public class ExcelController {
                                      .build().toString())
                              .contentType(MediaType.APPLICATION_OCTET_STREAM)
                              .body(resource);
+    }
+
+    @SneakyThrows
+    @GetMapping("/excel/xssf/encrypt")
+    public ResponseEntity<Resource> downloadXssfEncryptExcelFile() {
+        InputStreamResource resource = new InputStreamResource(service.getXssfEncryptExcelFile());
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, ContentDisposition.attachment()
+                        .filename("excel_encrypt.xlsx", StandardCharsets.UTF_8)
+                        .build().toString())
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(resource);
     }
 
 }
--- src/main/java/dev/proceane/excel/service/ExcelService.java
+++ src/main/java/dev/proceane/excel/service/ExcelService.java
@@ -1,15 +1,21 @@
 package dev.proceane.excel.service;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-
+import org.apache.poi.poifs.crypt.EncryptionInfo;
+import org.apache.poi.poifs.crypt.EncryptionMode;
+import org.apache.poi.poifs.crypt.Encryptor;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.streaming.SXSSFCell;
 import org.apache.poi.xssf.streaming.SXSSFRow;
 import org.apache.poi.xssf.streaming.SXSSFSheet;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
+
+import java.io.*;
+import java.security.GeneralSecurityException;
 
 @Service
 public class ExcelService {
@@ -25,5 +31,35 @@ public class ExcelService {
         workbook.write(outputStream);
         workbook.close();
         return new ByteArrayInputStream(outputStream.toByteArray());
+    }
+
+    public InputStream getXssfEncryptExcelFile() throws IOException, GeneralSecurityException {
+        XSSFWorkbook workbook = getXSSFWorkbook();
+
+        EncryptionInfo info = new EncryptionInfo(EncryptionMode.agile);
+        Encryptor enc = info.getEncryptor();
+        enc.confirmPassword("1234");
+
+        POIFSFileSystem fs = new POIFSFileSystem();
+        OutputStream os = enc.getDataStream(fs);
+        workbook.write(os);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        fs.writeFilesystem(outputStream);
+
+        os.close();
+        workbook.close();
+        fs.close();
+
+        return new ByteArrayInputStream(outputStream.toByteArray());
+    }
+
+    private XSSFWorkbook getXSSFWorkbook() {
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet();
+        Row row = sheet.createRow(0);
+        Cell cell = row.createCell(0);
+        cell.setCellValue("test");
+        return workbook;
     }
 }
